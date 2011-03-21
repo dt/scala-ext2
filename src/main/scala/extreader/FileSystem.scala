97c500cc6ade9f4e9c5badc9a843e90069f1f20c
David Taylor <dltaylor@andrew.cmu.edu>
2011-03-21 16:12:33 -0400

    tidy up a little

--- src/main/scala/extreader/FileSystem.scala
+++ src/main/scala/extreader/FileSystem.scala
@@ -7,40 +7,30 @@ object FileSystem {
 class FileSystem(val bytes: Bytes, sb: Superblock, val clean: Option[Bytes]) {
 	var superblock : Option[Superblock] = None
 
-	val metaBytes = clean.getOrElse(bytes)
 	val blockCache = collection.mutable.Map[Long, Block]()
 	val groupCache = collection.mutable.Map[Long, Group]()
 	val inodeCache = collection.mutable.Map[Long, Inode]()
 
 	def blockSizeExpo = sb.logBlockSize
 
-	var blocksPerGroup = sb.blocksPerGroup // ext2: 8192 // 8 * blockSize ?
+	def blocksPerGroup = sb.blocksPerGroup // ext2: 8192 // 8 * blockSize ?
 
-	var inodesPerGroup = sb.inodesPerGroup // ext2: 1832 // ext3: 1920
+	def inodesPerGroup = sb.inodesPerGroup // ext2: 1832 // ext3: 1920
 
-	var inodeCount = sb.inodeCount // ext2: 12824 // ext3: 19200
-	var blockCount = sb.blockCount // ext2: 51200 // ext3: 76800
+	def inodeCount = sb.inodeCount // ext2: 12824 // ext3: 19200
+	def blockCount = sb.blockCount // ext2: 51200 // ext3: 76800
 
-	var inodeBlocksPerGroup = sb.inodesPerGroup // ext2: 229
+	def inodeBlocksPerGroup = sb.inodesPerGroup // ext2: 229
 
-	var firstDataBlock = 1
+	def firstDataBlock = sb.firstBlock
 
-	val groups = blockCount / blocksPerGroup
+	def groups = blockCount / blocksPerGroup
 
 	def blockSize = 1024 << blockSizeExpo
 
-	val inodesPerBlock = blockSize / inodeSize
+	def inodesPerBlock = blockSize / inodeSize
 
 	def intsPerBlock = blockSize / 4
-
-	def sparseMetadata = false
-	
-	def metadataInGroup(groupNum: Int) = {
-		if (sparseMetadata)
-			(groupNum <= 1 || (groupNum isPowerOf 3) || (groupNum isPowerOf 5) || (groupNum isPowerOf 7))
-    else
-    	true
-  }
 
   def inodeSize = sb.inodeSize
 
@@ -55,12 +45,7 @@ class FileSystem(val bytes: Bytes, sb: Superblock, val clean: Option[Bytes]) {
 
 	def block(num: Long): Block = { 
 		debug("[fs]\tfetch block "+num)
-		 
 		blockCache.getOrElseUpdate(num, blockAt(num, num * blockSize))
-	}
-
-	def metaBlock(num: Long): Block = {
-		metaBlockAt(num, num * blockSize)
 	}
 
 	def inode(num: Long) = inodeCache.getOrElseUpdate(num, loadInode(num))
@@ -70,7 +55,7 @@ class FileSystem(val bytes: Bytes, sb: Superblock, val clean: Option[Bytes]) {
 			throw new IllegalArgumentException("Bad Inode Number: "+num)
 		}
 		debug("[fs]\tinode "+num+" is the "+inodeIndexInBlock(num)+" inode in group "+groupNumOfInode(num))
-		
+
 		val inodeBytes = group(groupNumOfInode(num)).inodeBytes(inodeIndexInBlock(num))
 
 		new Inode(this, num, inodeBytes)
@@ -90,8 +75,13 @@ class FileSystem(val bytes: Bytes, sb: Superblock, val clean: Option[Bytes]) {
 		new Block(num, bytes.getRange(at, blockSize) )
 	}
 
-	def metaBlockAt(num: Long, at: Long) = {
-		new Block(num, metaBytes.getRange(at, blockSize))
+	def metaBlock(num: Long): Block = {
+		metaBlockAt(num, num * blockSize)
 	}
+
+	def metaBlockAt(num: Long, at: Long) = {
+		new Block(num, clean.getOrElse(bytes).getRange(at, blockSize))
+	}
+
 	def fakeInodeAt(at: Long) = new Inode( this, -1, bytes.getRange(at, inodeSize))
 }
