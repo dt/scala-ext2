a997e71aff9e7d0eaa90b162d82ea0b95067828d
Daniel Freeman <dfreeman@cmu.edu>
2011-03-24 23:46:19 -0400

    Journal comments

--- src/main/scala/extreader/Journal.scala
+++ src/main/scala/extreader/Journal.scala
@@ -16,6 +16,17 @@ class Journal(bytes: Bytes) {
 }
 
 
+/* Table 4.1: Journal administrative block standard header 
+ *
+ * Range	Description	Values 
+ * 0-3		Signature	0xc03b3998 
+ * 4-7		Block type  1 Descriptor 
+ * 						2 Commit 
+ * 						3 Superblock Version1 
+ * 						4 Superblock Version 2 
+ * 						5 Revoke   
+ * 8-11  	Seq num  	Any
+ */
 object JournalHeader {
 	val T_DESCRIPTOR = 0x1L
 	val T_COMMIT = 0x2L
@@ -35,6 +46,17 @@ class JournalHeader(bytes: Bytes) {
 	}
 }
 
+
+/* Table 4.2: Journal Superblock 
+ *
+ * Byte Range	Description 
+ * 0-11  		Standard Header 
+ * 12-15  		Journal Block Size 
+ * 16-19  		Number of Journal blocks 
+ * 20-23  		Journal block where the journal actually start 
+ * 24-27  		Sequence number of first transaction 
+ * 28-31  		Journal block of first transaction
+ */
 class JournalSuperblock(bytes: Bytes) {
 	def header = new JournalHeader(bytes.getRange(0,12))
 	def blockSize = bytes.get4BE(12)
@@ -44,16 +66,36 @@ class JournalSuperblock(bytes: Bytes) {
 	def firstTransBlock = bytes.get4BE(28)
 }
 
+/* Table 4.3: Journal descriptor block 
+ * Byte Range  	Description		Values 
+ * 0-11  		Header  		See table 1 
+ * 12-15  		FS block  		Any 
+ * 16-19  		Entry flags  	0x01 Journal block has escaped (Note 1) 
+ * 								0x02 Entry has the same UUID as the previous (SAME_UUID) 
+ * 								0x04 Block was deleted by current transaction (currently not in use) 
+ * 								0x08 Last entry in descriptor block 
+ * 20-23  		UUID 			(Does not exist if SAME_UUID flag is set)
+ */
 class JournalDescriptor(bytes: Bytes) {
 	def header = new JournalHeader(bytes.getRange(0,12))
 	def fsBlock = bytes.get4BE(12)
 	def flags = bytes.get4BE(16)
 }
 
+/* Table 4.4: Journal commit block 
+ * Byte Range	Description 
+ * 0-11  		Standard Header
+ */
 class JournalCommit(bytes: Bytes) {
 	def header = new JournalHeader(bytes.getRange(0,12))
 }
 
+/* Table 4.5: Journal revoke block 
+ * Byte Range	Description 
+ * 0-11  		Standard Header 
+ * 12-15  		Size in bytes of revoke data 
+ * 16-SIZE  	List of 4-byte file system block addresses being revoked 
+ */
 class JournalRevoke(bytes: Bytes) {
 	def header = new JournalHeader(bytes.getRange(0,12))
 	def size = bytes.get4BE(12)
